838dd7f063293a8e07fe4409ee1d4fca736698c8
Josh Cai <josh.cai@yahoo.com>
2013-10-04 12:16:07 -0500

    lots of changes

--- blog/views.py
+++ blog/views.py
@@ -13,9 +13,45 @@ import hashlib
 
 from blog.models import User, Post
 
-# from resume_app.models import Users
+def render_post(content):
+	array_str = content.splitlines()
+	new_content = ""
+	for temp in array_str:
+		if temp=="":
+			new_content+="<br />\n"
+		else:
+			new_content+="<p>"+temp+"</p>\n"
+	return new_content
+
+def newpost(request):
+	if 'user' in request.session:
+		if request.method == 'POST':
+			if request.POST['subject'] and request.POST['content']:
+				d = datetime.datetime.now()
+				p = Post(subject=request.POST['subject'], content=request.POST['content'], content_rendered=render_post(request.POST['content']), date=d, date_str=d.strftime('%B %d, %Y'))
+				p.save()
+				return HttpResponseRedirect(reverse('blog:blog'))
+			else:
+				context={
+					'subject': request.POST['subject'],
+					'content': request.POST['content'],
+					'error_message': "Please fill in all fields",
+					'request': request,
+				}
+				return render(request, 'blog/newpost.html', context)
+		return render(request, 'blog/newpost.html', {'request': request})
+	else:
+		return HttpResponseRedirect(reverse('blog:index'))
+
 def index(request):
-	return render(request, 'blog/index.html', {'request':request})
+	return render(request, 'blog/index.html', {'request':request, 'nav': 'home'})
+
+def blog(request):
+	posts = Post.objects.order_by('-date')
+	return render(request, 'blog/blog.html', {'request':request, 'nav': 'blog', 'posts': posts})
+
+def about(request):
+	return render(request, 'blog/about.html', {'request':request, 'nav': 'about'})
 
 def signup(request):
 	if request.method == 'POST':
@@ -36,9 +72,9 @@ def signup(request):
 				u.save()
 				request.session['user'] = u.username
 				return HttpResponseRedirect(reverse('blog:index'))
-			context['error_message'] = 'Password mismatch <br />'
+			context['error_message'] = 'Password mismatch'
 		else:
-			context['error_message'] = 'Please fill in all fields <br />'
+			context['error_message'] = 'Please fill in all fields'
 		return render(request, 'blog/signup.html', context)
 	return render(request, 'blog/signup.html', {'request': request})
 
@@ -59,7 +95,7 @@ def login(request):
 			request.session['user'] = user.username
 			return HttpResponseRedirect(reverse('blog:index'))
 		context['username'] = request.POST['username']
-		context['error_message'] = "Username or password incorrect <br/>"
+		context['error_message'] = "Username or password incorrect"
 	return render(request, 'blog/login.html', context)
 
 
